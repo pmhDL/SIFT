a5490e76de5516f6d821e4b12464012134883a8d
pmhDL <65716615+pmhDL@users.noreply.github.com>
2023-09-26 20:41:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 from utils.misc import pprint
 from utils.gpu_tools import set_gpu
 from trainer.st import ST
+from trainer.ns import NS
 from trainer.dc import DC
 from trainer.lr import LR
 
@@ -13,18 +14,18 @@ warnings.filterwarnings("ignore")
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Basic parameters
-    parser.add_argument('--model_type', type=str, default='res12', choices=['res12', 'wrn28']) # The network architecture
-    parser.add_argument('--dataset', type=str, default='mini', choices=['mini', 'tiered', 'cub', 'cifar_fs']) # Dataset
-    parser.add_argument('--phase', type=str, default='st_te', choices=['st_te', 'lr_te', 'dc_te']) # Phase
+    parser.add_argument('--model_type', type=str, default='res12', choices=['res12', 'wrn28'])
+    parser.add_argument('--dataset', type=str, default='mini', choices=['mini', 'tiered', 'cub', 'cifar_fs'])
+    parser.add_argument('--phase', type=str, default='st_te', choices=['st_te', 'ns_te', 'lr_te', 'dc_te'])
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--gpu', default='1') # GPU id
-    parser.add_argument('--dataset_dir', type=str, default='./data/mini/res12') # Dataset folder
-    # Parameters for meta-train phase
-    parser.add_argument('--shot', type=int, default=1) # Shot number, how many samples for one class in a task
-    parser.add_argument('--way', type=int, default=5) # Way number, how many classes in a task
-    parser.add_argument('--train_query', type=int, default=15) # The number of training samples for each class in a task
-    parser.add_argument('--val_query', type=int, default=15) # The number of test samples for each class in a task
-    parser.add_argument('--num_aug', type=int, default=1) #number of augmented samples per class
+    parser.add_argument('--gpu', default='1')
+    parser.add_argument('--dataset_dir', type=str, default='/data/FSL/mini')
+
+    parser.add_argument('--shot', type=int, default=1)
+    parser.add_argument('--way', type=int, default=5)
+    parser.add_argument('--train_query', type=int, default=15)
+    parser.add_argument('--val_query', type=int, default=15)
+    parser.add_argument('--num_aug', type=int, default=1)
     parser.add_argument('--classifiermethod', type=str, default='gradient', choices=['metric', 'nonparam', 'gradient'])
     parser.add_argument('--cls', type=str, default='lr', choices=['lr', 'svm', 'knn'])
     parser.add_argument('--selectm', type=str, default='randomselect', choices=['nn2suppcenter', 'nn2basecenter', 'randomselect'])
@@ -47,9 +48,12 @@ if __name__ == '__main__':
         torch.cuda.manual_seed(args.seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
-    # Start trainer for pre-train, meta-train or meta-eval
+    # Start training
     if args.phase == 'st_te':
         trainer = ST(args)
+        trainer.eval()
+    elif args.phase == 'ns_te':
+        trainer = NS(args)
         trainer.eval()
     elif args.phase == 'lr_te':
         trainer = LR(args)
